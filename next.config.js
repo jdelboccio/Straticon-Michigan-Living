fc0b45d594730f0590163d829a21cb422f975fee
CodeSandbox <support@codesandbox.io>
2025-05-23 20:53:41 +0000

    Update next.config.js and fix image configurations

--- next.config.js
+++ next.config.js
@@ -4,16 +4,26 @@ const nextConfig = {
   basePath: '/Straticon-Michigan-Living',
   images: {
     unoptimized: true,
-    domains: ['images.pexels.com'],
+    domains: [
+      'images.pexels.com',
+      'cdn.traversecity.com',
+      'images.unsplash.com'
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**'
+      }
+    ]
   },
   // Disable server-side features for static export
   experimental: {
-    appDir: true,
+    appDir: true
   },
   // Disable features that require server-side functionality
   typescript: {
-    ignoreBuildErrors: true,
-  },
+    ignoreBuildErrors: true
+  }
 }
 
 module.exports = nextConfig
